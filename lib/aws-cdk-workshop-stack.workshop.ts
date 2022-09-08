9e1bb76f18ade71e1f341e8ade117054c6725282
Daniel Bartholomae <daniel.bartholomae@optilyz.com>
2022-09-08 17:02:32 +0200

    Show notes in lambda

--- lib/aws-cdk-workshop-stack.workshop.ts
+++ lib/aws-cdk-workshop-stack.workshop.ts
@@ -1,6 +1,12 @@
+import S3 from 'aws-sdk/clients/s3';
+
+const s3 = new S3()
+
 export async function handler() {
+  const objects = await s3.listObjects({ Bucket: process.env.BUCKET_NAME! }).promise()
+  const notes = objects.Contents!.map(content => content.Key);
   return {
     statusCode: 200,
-    body: "Hello optilyz!"
+    body: notes.length === 0 ? "No notes" : notes.join(', ')
   };
 }
