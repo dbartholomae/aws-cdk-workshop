fff03dd1fc5e27e01e6d9648da93353c073621d8
Daniel Bartholomae <daniel.bartholomae@optilyz.com>
2022-09-08 15:17:58 +0200

    Initial commit

---
+++ test/aws-cdk-workshop.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkWorkshop from '../lib/aws-cdk-workshop-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-workshop-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkWorkshop.AwsCdkWorkshopStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
