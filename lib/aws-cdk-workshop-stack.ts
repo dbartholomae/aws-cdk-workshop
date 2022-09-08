29ac50a61703c44fc66f5b6887bd34aae6feefcd
Daniel Bartholomae <daniel.bartholomae@optilyz.com>
2022-09-08 16:50:26 +0200

    Add bucket

--- lib/aws-cdk-workshop-stack.ts
+++ lib/aws-cdk-workshop-stack.ts
@@ -1,13 +1,24 @@
 import * as cdk from "aws-cdk-lib";
+import { CfnOutput, RemovalPolicy } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { FunctionUrlAuthType } from "aws-cdk-lib/aws-lambda";
-import { CfnOutput } from "aws-cdk-lib";
+import { Bucket } from "aws-cdk-lib/aws-s3";
 
 export class AwsCdkWorkshopStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    const fn = new NodejsFunction(this, "workshop");
+    const bucket = new Bucket(this, 'Bucket', {
+      removalPolicy: RemovalPolicy.DESTROY
+    })
+
+    const fn = new NodejsFunction(this, "workshop", {
+      environment: {
+        BUCKET_NAME: bucket.bucketName
+      }
+    });
+    bucket.grantRead(fn);
+
     const fnUrl = fn.addFunctionUrl({ authType: FunctionUrlAuthType.NONE });
 
     new CfnOutput(this, "lambdaUrl", { value: fnUrl.url });
